2ab97f94180ece8cb0d0b3bb312083730a00783c
Kevin Mai <kevinmai@Kevins-MacBook-Pro.local>
2024-02-05 12:51:59 -0500

    able to select, play and pause silly songs

---
+++ assets/music.js
@@ -0,0 +1,97 @@
+export const music = [
+  {
+    title: "Alvin And The Chipmunks - Turn Down For What",
+    image: require("./sillysongs/Alvin And The Chipmunks - Turn Down For What.jpg"),
+    mp3: require("./sillysongs/Alvin And The Chipmunks - Turn Down For What.mp3")
+  },
+  {
+    title: "Children's Classics - The Hamster Dance",
+    image: require("./sillysongs/Children's Classics - The Hamster Dance.jpg"),
+    mp3: require("./sillysongs/Children's Classics - The Hamster Dance.mp3")
+  },
+  {
+    title: "Chompers - Bees Can Talk by Moving Their Butts",
+    image: require("./sillysongs/Chompers - Bees Can Talk by Moving Their Butts.jpg"),
+    mp3: require("./sillysongs/Chompers - Bees Can Talk by Moving Their Butts.mp3")
+  },
+  {
+    title: "Chompers - Do the Robot",
+    image: require("./sillysongs/Chompers - Do the Robot.jpg"),
+    mp3: require("./sillysongs/Chompers - Do the Robot.mp3")
+  },
+  {
+    title: "Cooltime - B-I-N-G-O",
+    image: require("./sillysongs/Cooltime - B-I-N-G-O.jpg"),
+    mp3: require("./sillysongs/Cooltime - B-I-N-G-O.mp3")
+  },
+  {
+    title: "Fast Food Rockers - Fast Food Song",
+    image: require("./sillysongs/Fast Food Rockers - Fast Food Song.jpg"),
+    mp3: require("./sillysongs/Fast Food Rockers - Fast Food Song.mp3")
+  },
+  {
+    title: "GoNoodle, Moose Tube - Gobble Gobble Burp - Live",
+    image: require("./sillysongs/GoNoodle, Moose Tube - Gobble Gobble Burp - Live.jpg"),
+    mp3: require("./sillysongs/GoNoodle, Moose Tube - Gobble Gobble Burp - Live.mp3")
+  },
+  {
+    title: "Gummibär - I Am Your Gummy Bear",
+    image: require("./sillysongs/Gummibär - I Am Your Gummy Bear.jpg"),
+    mp3: require("./sillysongs/Gummibär - I Am Your Gummy Bear.mp3")
+  },
+  {
+    title: "Parry Gripp - Raining Tacos",
+    image: require("./sillysongs/Parry Gripp - Raining Tacos.jpg"),
+    mp3: require("./sillysongs/Parry Gripp - Raining Tacos.mp3")
+  },
+  {
+    title: "PIKOTARO - PPAP (Pen Pineapple Apple Pen)",
+    image: require("./sillysongs/PIKOTARO - PPAP (Pen Pineapple Apple Pen).jpg"),
+    mp3: require("./sillysongs/PIKOTARO - PPAP (Pen Pineapple Apple Pen).mp3")
+  },
+  {
+    title: "Pinkfong - Animals Sound Fun",
+    image: require("./sillysongs/Pinkfong - Animals Sound Fun.jpg"),
+    mp3: require("./sillysongs/Pinkfong - Animals Sound Fun.mp3")
+  },
+  {
+    title: "Regurgitator's Pogogo Show - Mr Butt",
+    image: require("./sillysongs/Regurgitator's Pogogo Show - Mr Butt.jpg"),
+    mp3: require("./sillysongs/Regurgitator's Pogogo Show - Mr Butt.mp3")
+  },
+  {
+    title: "Sandy Cheeks with Junior Brown - Texas Song",
+    image: require("./sillysongs/Sandy Cheeks with Junior Brown - Texas Song.jpg"),
+    mp3: require("./sillysongs/Sandy Cheeks with Junior Brown - Texas Song.mp3")
+  },
+  {
+    title: "Scatman John - Scatman (ski-ba-bop-ba-dop-bop)",
+    image: require("./sillysongs/Scatman John - Scatman (ski-ba-bop-ba-dop-bop).jpg"),
+    mp3: require("./sillysongs/Scatman John - Scatman (ski-ba-bop-ba-dop-bop).mp3")
+  },
+  {
+    title: "Spongebob Squarepants, Plankton - F.U.N. Song",
+    image: require("./sillysongs/Spongebob Squarepants, Plankton - F.U.N. Song.jpg"),
+    mp3: require("./sillysongs/Spongebob Squarepants, Plankton - F.U.N. Song.mp3")
+  },
+  {
+    title: "The Minions - Y.M.C.A.",
+    image: require("./sillysongs/The Minions - Y.M.C.A..jpg"),
+    mp3: require("./sillysongs/The Minions - Y.M.C.A..mp3")
+  },
+  {
+    title: "The Wiggles - Toot Toot, Chugga Chugga, Big Red Car",
+    image: require("./sillysongs/The Wiggles - Toot Toot, Chugga Chugga, Big Red Car.jpg"),
+    mp3: require("./sillysongs/The Wiggles - Toot Toot, Chugga Chugga, Big Red Car.mp3")
+  },
+  {
+    title: "Tim Rothrock & Jim Wise - Goofy Goober Rock",
+    image: require("./sillysongs/Tim Rothrock & Jim Wise - Goofy Goober Rock.jpg"),
+    mp3: require("./sillysongs/Tim Rothrock & Jim Wise - Goofy Goober Rock.mp3")
+  },
+  {
+    title: "VeggieTales - VeggieTales Theme Song",
+    image: require("./sillysongs/VeggieTales - VeggieTales Theme Song.jpg"),
+    mp3: require("./sillysongs/VeggieTales - VeggieTales Theme Song.mp3")
+  }
+]
--- src/pages/createvideo.js
+++ src/pages/createvideo.js
@@ -1,19 +1,29 @@
 import { useEffect, useState, useRef } from "react";
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { ActivityIndicator, Platform, Dimensions, SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet, Modal, ScrollView } from "react-native";
 import { Camera, useCameraDevice } from "react-native-vision-camera";
 import { Iconify } from 'react-native-iconify';
 import Pie from 'react-native-pie'
+import { Audio } from 'expo-av';
+import { music } from "../../assets/music"
+
+const { width, height } = Dimensions.get('window')
 
 export default function Createvideo({ navigation }) {
   const [permissions, setPermissions] = useState({ camera: false, audio: false })
   const [type, setType] = useState('back')
   const [flashOn, setFlashon] = useState(false)
+  const [sound, setSound] = useState(null)
   const camera = useRef(null)
   const device = useCameraDevice(type);
   const [recordStatus, setRecordstatus] = useState(0)
   const [firstStart, setFirststart] = useState(false)
   const [recording, setRecording] = useState(false)
+  const [selectedMusic, setSelectedmusic] = useState(null)
+  const [media, setMedia] = useState({ show: false, name: '', width: 0, height: 0 })
   const timer = useRef(null)
+
+  // hiddens
+  const [musicList, setMusiclist] = useState({ show: false, list: [...music] })
 
   const counter = () => {
     setRecordstatus(prev => {
@@ -48,7 +58,48 @@ export default function Createvideo({ navigation }) {
     await camera.current.stopRecording()
   }
   const takePhoto = async() => {
-    alert("take photo")
+    const photo = await camera.current.takePhoto({
+      qualityPrioritization: 'speed',
+      flash: 'off',
+      enableShutterSound: false
+    })
+    let path = photo.path, width = photo.height, height = photo.width
+    let cropy = 0, uri = Platform.OS == "ios" ? path : `file://${path}`
+    let options = []
+
+    console.log(photo)
+  }
+  const selectMusic = index => {
+    setSelectedmusic(music[index])
+    setMusiclist({ ...musicList, show: false })
+  }
+  const playMusic = async index => {
+    const newList = [...musicList.list]
+    
+    // show loading music
+    newList[index]["loading"] = true
+    setMusiclist({ ...musicList, list: newList })
+
+    const { sound } = await Audio.Sound.createAsync(musicList.list[index].mp3)
+
+    delete newList[index]["loading"]
+    newList[index]["play"] = true
+
+    setMusiclist({ ...musicList, list: newList })
+    setSound(sound)
+
+    await sound.playAsync();
+  }
+  const stopMusic = async index => {
+    const newList = [...musicList.list]
+
+    delete newList[index]["play"]
+
+    setMusiclist({ ...musicList, list: newList })
+
+    sound.unloadAsync()
+
+    setSound(null)
   }
 
   useEffect(() => {
@@ -90,7 +141,7 @@ export default function Createvideo({ navigation }) {
       ref={camera}
       device={device}
       isActive
-      video audio
+      photo video audio
     >
       <SafeAreaView style={styles.box}>
         <View style={styles.createVideo}>
@@ -142,9 +193,69 @@ export default function Createvideo({ navigation }) {
                 <View style={styles.recordKnob}/>
               </TouchableOpacity>
             }
+
+            <TouchableOpacity style={styles.addMusic} onPress={() => setMusiclist({ ...musicList, show: true })}>
+              <Iconify icon="tabler:music" size={30} color="white"/>
+            </TouchableOpacity>
           </View>
         </View>
       </SafeAreaView>
+
+      {musicList.show && (
+        <Modal transparent animationType="slide">
+          <View style={styles.hiddenBox}>
+            {musicList.show && (
+              <View style={styles.musicSelectionBox}>
+                <TouchableOpacity style={styles.musicSelectionClose} onPress={() => setMusiclist({ ...musicList, show: false })}>
+                  <Iconify icon="mingcute:close-fill" color="black" size={'100%'}/>
+                </TouchableOpacity>
+
+                <Text style={styles.musicSelectionHeader}>Select music for goofy video</Text>
+
+                <ScrollView style={styles.musicSelectionList}>
+                  {musicList.list.map((info, index) => (
+                    <TouchableOpacity style={styles.musicSelectionItem} key={"music-" + index} onPress={() => selectMusic(index)}>
+                      <View style={styles.musicSelectionItemImage}>
+                        <Image style={{ height: '100%', width: '100%' }} source={info.image}/>
+                      </View>
+                      <Text style={styles.musicSelectionItemHeader}>{info.title}</Text>
+
+                      {info.loading ? 
+                        <View style={styles.musicSelectionMediaToggle}>
+                          <ActivityIndicator size={'100%'} color="black"/>
+                        </View>
+                        :
+                        <TouchableOpacity style={styles.musicSelectionMediaToggle} onPress={() => {
+                          if (!info.play) {
+                            playMusic(index)
+                          } else {
+                            stopMusic(index)
+                          }
+                        }}>
+                          {info.play ? 
+                            <Iconify icon="zondicons:pause" color="black" size={"100%"}/>
+                            :
+                            <Iconify icon="mdi:play" color="black" size={'100%'}/>
+                          }
+                        </TouchableOpacity>
+                      }
+                      
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+              </View>
+            )}
+          </View>
+        </Modal>
+      )}
+
+      {media.show && (
+        <Modal transparent>
+          <View style={styles.selectedMediaBox}>
+
+          </View>
+        </Modal>
+      )}
     </Camera>
   )
 }
@@ -157,9 +268,25 @@ const styles = StyleSheet.create({
   topNavsRow: { flexDirection: 'row', justifyContent: 'space-around' },
   topNav: { marginHorizontal: '5%' },
 
-  bottomNavs: { alignItems: 'center', width: '100%' },
+  bottomNavs: { alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginHorizontal: '25%', width: '50%' },
   recordingTouch: { alignItems: 'center', borderColor: 'white', borderRadius: 40, borderStyle: 'solid', borderWidth: 3, height: 80, justifyContent: 'center', width: 80 },
   recordingKnob: { backgroundColor: 'white', borderRadius: 40, height: 60, marginTop: 10, position: 'absolute', width: 60 },
   recordTouch: { alignItems: 'center', borderColor: 'white', borderRadius: 40, borderStyle: 'solid', borderWidth: 8, height: 80, justifyContent: 'center', width: 80 },
   recordKnob: { backgroundColor: 'white', borderRadius: 40, height: '95%', width: '95%' },
+
+  // hiddens
+  hiddenBox: { backgroundColor: 'rgba(0, 0, 0, 0.7)', flexDirection: 'column-reverse', height: '100%', justifyContent: 'space-between', width: '100%' },
+
+  // music selection box
+  musicSelectionBox: { backgroundColor: 'white', borderTopLeftRadius: 50, borderTopRightRadius: 50, height: '85%', width: '100%' },
+  musicSelectionClose: { height: 30, marginLeft: width - 50, marginTop: 20, width: 30 },
+  musicSelectionHeader: { fontFamily: 'DeliusSwashCaps', fontSize: 20, marginBottom: 20, textAlign: 'center' },
+  musicSelectionList: { height: '100%', width: '100%' },
+  musicSelectionItem: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10, paddingHorizontal: 10 },
+  musicSelectionItemImage: { borderRadius: 25, height: 50, overflow: 'hidden', width: 50 },
+  musicSelectionItemHeader: { fontSize: 15, paddingRight: 10, width: width - 105 },
+  musicSelectionMediaToggle: { borderRadius: 15, borderStyle: 'solid', borderWidth: 1, height: 30, width: 30 },
+
+  // selected media box
+  selectedMediaBox: { backgroundColor: 'white', height: '100%', width: '100%' },
 })
